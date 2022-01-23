d0b2baa130fefd4d1d7f6fa3e0a90c698a0fd42c
akash028 <akash.bhapkar028@gmail.com>
2022-01-22 21:13:46 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
 # main.py: Driver program to test machine learning implementations on various datasets.
-#
-# Based on skeleton code by CSCI-B 551 Fall 2021 Course Staff
-#
-# Feel free to edit this driver program file in order to test other datasets or change specific parameters. When
-# grading, we will use our own driver program and test cases to test your machine learning implementations and functions
-# made from scratch.
 
 import sys
 
@@ -118,118 +112,6 @@ def test_knn():
     print('Done Testing K-Nearest Neighbors Classification!\n')
 
 
-def test_mlp():
-    print('Testing Multilayer Perceptron Classification...')
-    param_dict = {'n_hidden': [8, 16, 32], 'hidden_activation': ['identity', 'sigmoid', 'tanh', 'relu'],
-                  'n_iterations': [500, 1000, 2000], 'learning_rate': [0.1, 0.01, 0.001]}
-    params_list = [dict(zip(param_dict.keys(), v)) for v in product(*param_dict.values())]
-    params_len = len(params_list)
-
-    mlp_iris_results = pd.DataFrame(columns = ['  Model',
-                                               '  # of Hidden Neurons',
-                                               '  Hidden Activation Function',
-                                               '  # of Iterations',
-                                               '  Learning Rate',
-                                               '  Accuracy Score'])
-
-    for i, params in enumerate(params_list):
-        sys.stdout.write('\r')
-        sys.stdout.write(f" - Iris Dataset Progress:   [{'=' * int(60 * (i + 1) / params_len):60s}]"
-                         f' {int(100 * (i + 1) / params_len)}%')
-        sys.stdout.flush()
-
-        param_logistic = params['hidden_activation'] == 'sigmoid'
-        sklearn_model = MLPClassifier((params['n_hidden'],),
-                                      params['hidden_activation'] if not param_logistic else 'logistic',
-                                      solver = 'sgd',
-                                      alpha = 0,
-                                      batch_size = X_train_i.shape[0],
-                                      learning_rate_init = params['learning_rate'],
-                                      max_iter = params['n_iterations'],
-                                      shuffle = False,
-                                      random_state = 42)
-        sklearn_model.fit(X_train_i, y_train_i)
-        y_pred = sklearn_model.predict(X_test_i)
-
-        mlp_iris_results.loc[len(mlp_iris_results)] = ['Sklearn',
-                                                       params['n_hidden'],
-                                                       params['hidden_activation'],
-                                                       params['n_iterations'],
-                                                       params['learning_rate'],
-                                                       round(accuracy_score(y_test_i, y_pred), 3)]
-
-        my_model = MultilayerPerceptron(params['n_hidden'], params['hidden_activation'],
-                                        params['n_iterations'], params['learning_rate'])
-        my_model.fit(X_train_i, y_train_i)
-        y_pred = my_model.predict(X_test_i)
-
-        mlp_iris_results.loc[len(mlp_iris_results)] = ['My Model',
-                                                       params['n_hidden'],
-                                                       params['hidden_activation'],
-                                                       params['n_iterations'],
-                                                       params['learning_rate'],
-                                                       round(accuracy_score(y_test_i, y_pred), 3)]
-
-        mlp_iris_results.loc[len(mlp_iris_results)] = ['', '', '', '', '', '']
-
-    print()
-
-    mlp_digits_results = pd.DataFrame(columns = ['  Model',
-                                                 '  # of Hidden Neurons',
-                                                 '  Hidden Activation Function',
-                                                 '  # of Iterations',
-                                                 '  Learning Rate',
-                                                 '  Accuracy Score'])
-
-    for i, params in enumerate(params_list):
-        sys.stdout.write('\r')
-        sys.stdout.write(f" - Digits Dataset Progress: [{'=' * int(60 * (i + 1) / params_len):60s}]"
-                         f' {int(100 * (i + 1) / params_len)}%')
-        sys.stdout.flush()
-
-        param_logistic = params['hidden_activation'] == 'sigmoid'
-        sklearn_model = MLPClassifier((params['n_hidden'],),
-                                      params['hidden_activation'] if not param_logistic else 'logistic',
-                                      solver = 'sgd',
-                                      alpha = 0,
-                                      batch_size = X_train_i.shape[0],
-                                      learning_rate_init = params['learning_rate'],
-                                      max_iter = params['n_iterations'],
-                                      shuffle = False,
-                                      random_state = 42)
-        sklearn_model.fit(X_train_d, y_train_d)
-        y_pred = sklearn_model.predict(X_test_d)
-
-        mlp_digits_results.loc[len(mlp_digits_results)] = ['Sklearn',
-                                                           params['n_hidden'],
-                                                           params['hidden_activation'],
-                                                           params['n_iterations'],
-                                                           params['learning_rate'],
-                                                           round(accuracy_score(y_test_d, y_pred), 3)]
-
-        my_model = MultilayerPerceptron(params['n_hidden'], params['hidden_activation'],
-                                        params['n_iterations'], params['learning_rate'])
-        my_model.fit(X_train_d, y_train_d)
-        y_pred = my_model.predict(X_test_d)
-
-        mlp_digits_results.loc[len(mlp_digits_results)] = ['My Model',
-                                                           params['n_hidden'],
-                                                           params['hidden_activation'],
-                                                           params['n_iterations'],
-                                                           params['learning_rate'],
-                                                           round(accuracy_score(y_test_d, y_pred), 3)]
-
-        mlp_digits_results.loc[len(mlp_digits_results)] = ['', '', '', '', '', '']
-
-    print('\n')
-
-    print('Exporting MLP Results to HTML Files...\n')
-    mlp_iris_results.to_html('mlp_iris_results.html')
-    mlp_digits_results.to_html('mlp_digits_results.html')
-
-    print('Done Testing Multilayer Perceptron Classification!\n')
-
-
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         raise Exception('Incorrect number of arguments have been specified.')
@@ -259,13 +141,7 @@ if __name__ == '__main__':
     X_test_i = sc_i.transform(X_test_i)
     X_train_d = sc_d.fit_transform(X_train_d)
     X_test_d = sc_d.transform(X_test_d)
-
-    if sys.argv[1] == 'knn':
-        test_knn()
-    elif sys.argv[1] == 'mlp':
-        test_mlp()
-    else:
-        test_knn()
-        test_mlp()
+    
+    test_knn()
 
     print('Program Finished! Exiting the Program...')
